415cd53fff4f624922b59e0f5308bb8f514bda17
quiram <abraham.marin.perez@gmail.com>
2015-04-19 14:41:50 +0100

    Creating project layout

---
+++ src/main/java/com/amarinperez/pseudotwitter/App.java
@@ -0,0 +1,13 @@
+package com.amarinperez.pseudotwitter;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
